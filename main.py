f740034da80c4b3a67acd36d8be631de2385f4df
firat <krizalit@gmail.com>
2023-04-07 20:20:22 +0300

    solda sembolleri seçince ortalığa basmaya başladı

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
-from kzmodul import *
+from PyQt5.QtGui import QBrush, QColor
+from kzmodul2 import *
 
 Uygulama = QApplication(sys.argv)
 kzmodulAnaPencere = QMainWindow()
@@ -23,72 +24,160 @@ baglanti = mysql.connector.connect(
 )
 vtimlec = baglanti.cursor()
 
-#sembol = 'ASELS'
-alimlariCek = "SELECT `gun`, `gerceklesen`, `fiyat`, `hacim` FROM `emirlerim` WHERE `sembol` = 'ASELS' AND `alsat` = 'A'"
-satimlariCek = "SELECT `gerceklesen`, `fiyat`, `hacim`, `gun` FROM `emirlerim` WHERE `sembol` = 'ASELS' AND `alsat` = 'S'"
+sembol = "ESEN"
 
+#alimlariCek = f"SELECT `gun`, `gerceklesen`, `fiyat`, `hacim` FROM `emirlerim` WHERE `sembol` = '{sembol}' AND `alsat` = 'A'"
+#satimlariCek = f"SELECT `gerceklesen`, `fiyat`, `hacim`, `gun` FROM `emirlerim` WHERE `sembol` = '{sembol}' AND `alsat` = 'S'"
+sembolleriCek = "SELECT `sembol` FROM `semboller` ORDER BY `semboller`.`sembol` ASC "
 #----------------------Tanımlamalar-------------------------#
-#sembol = "ASELS"
+
 toplamAdet = 0
+toplamAlimAdet = 0
+toplamSatimAdet = 0
 
-def listele_alim():
 
+def sembolleriYerlestir():
+  vtimlec.execute(sembolleriCek)
+  sembolGelenler = vtimlec.fetchall()
+
+  # QListWidget nesnesine sembolleri ekle
+  kzarayuz.listWidget_semboller.clear()
+  for smbl in sembolGelenler:
+    kzarayuz.listWidget_semboller.addItem(smbl[0])
+
+  # QListWidget nesnesindeki herhangi bir öğe tıklandığında seciliSembolDegistir fonksiyonunu çağır
+  kzarayuz.listWidget_semboller.itemClicked.connect(seciliSembolDegistir)
+  global sembol
+  kzarayuz.lineEdit_sembol.setText(str(sembol))
+
+def seciliSembolDegistir(item):
+  global sembol
+  sembol = item.text()
+  kzarayuz.lineEdit_sembol.setText(sembol)
+  alimVerisiIsleme()
+  satimVerisiIsleme()
+
+def sembolGonder():
+  kzarayuz.pushButton_sembolGonder.clicked()
+
+def alimVerisiIsleme():
   alimAdet = 0
   alimHacim = 0
-  vtimlec.execute(alimlariCek)
-  alimlar = vtimlec.fetchall()
-  alimlarSatirSayisi = vtimlec.rowcount
-  print(alimlarSatirSayisi)
-  for  a, b, c, d in alimlar:
-    alimAdet += b
-    alimHacim += d
 
+  # MySQL sorgusunu çalıştır
+  vtimlec.execute(f"SELECT `gun`, `gerceklesen`, `fiyat`, `hacim` FROM `emirlerim` WHERE `sembol` = '{sembol}' AND `alsat` = 'A'")
+  islemIcinGelenAlimlar = vtimlec.fetchall()
+
+  # Verileri işleme
+  alimVerisiDizeye = []
+  for satir in islemIcinGelenAlimlar:
+
+    # Toplam alım ve hacimi hesaplama - Dizeye ekleme esnasında yan işlem
+    alimAdet += satir[1]
+    hacim = satir[3] / 1000 * 1002
+    alimHacim += hacim
+
+    # Tarih verisini dd mm yyyy formatına çevir
+    tarih = satir[0].strftime("%d %m %Y")
+    # adet verisi tanımıyla gerceklenenleri adete çevir
+    adet = satir[1]
+
+    # Fiyat verisini binlik ayraçlı sayı formatına çevir ve virgülden sonra 2 basamak göster
+    fiyat = '{:,.2f}'.format(satir[2]).replace(",", "X").replace(".", ",").replace("X", ".")
+
+    # Eder verisini binlik ayraçlı sayı formatına çevir ve virgülden sonra 2 basamak göster
+    eder = '{:,.2f}'.format(hacim).replace(",", "X").replace(".", ",").replace("X", ".")
+
+    # Düzenlenmiş veriyi listeye ekle
+    alimVerisiDizeye.append([tarih, adet, fiyat, eder])
+
+  # QTableWidget nesnesine veriyi yerleştir
   kzarayuz.tableWidget_alim.clear()
   kzarayuz.tableWidget_alim.setHorizontalHeaderLabels(['Tarih', 'Adet', 'Fiyat', 'Eder'])
   kzarayuz.tableWidget_alim.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-  kzarayuz.tableWidget_alim.setRowCount(alimlarSatirSayisi)
-
-  for satirIndeks, satirVeri in enumerate(alimlar):
+  kzarayuz.tableWidget_alim.setRowCount(len(islemIcinGelenAlimlar))
+  for satirIndeks, satirVeri in enumerate(alimVerisiDizeye):
     for sutunIndeks, sutunVeri in enumerate(satirVeri):
-
       hucre = QTableWidgetItem(str(sutunVeri))
-      if isinstance(sutunVeri, float):
-        hucre.setData(Qt.EditRole, '{:,.2f}'.format(sutunVeri))
+      if sutunIndeks == 1:
 
-      if sutunIndeks > 0:
+        #Ortala
+        hucre.setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+      else:
+
+        # Sağa yasla
         hucre.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+      if sutunIndeks == 0:
+        hucre.setBackground(QBrush(QColor(242, 237, 221)))
+      # Hücreyi tabloya yerleştir
+      kzarayuz.tableWidget_alim.setItem(satirIndeks, sutunIndeks, hucre)
 
-      kzarayuz.tableWidget_alim.setItem(satirIndeks,sutunIndeks,hucre)
-
-  print("Toplam Alınan Adet:"+ str(alimAdet))
+  #Saydadaki diğer işlemlerde kullanılacak değişken değerlerinin oluşması ve yerleşmesi
+  print("Toplam Alınan Adet:" + str(alimAdet))
   alimOrtalamasi = alimHacim / alimAdet
   alimOrtalamasiSade = "{:.2f}".format(alimOrtalamasi)
-  print("Alım Ortalaması:"+ str(alimOrtalamasiSade))
+  print("Alım Ortalaması:" + str(alimOrtalamasiSade))
   kzarayuz.label_alimOrtalama.clear()
   kzarayuz.label_alimOrtalama.setText(str(alimOrtalamasiSade))
   global toplamAdet
   toplamAdet += alimAdet
+  global toplamAlimAdet
+  toplamAlimAdet += alimAdet
 
 
-def listele_satim():
+def satimVerisiIsleme():
   satimAdet = 0
   satimHacim = 0
 
-  vtimlec.execute(satimlariCek)
-  satimlar = vtimlec.fetchall()
-  satimlarSatirSayisi = vtimlec.rowcount
-  for  a, b, c, d in satimlar:
-    satimAdet += a
-    satimHacim += c
-  print(satimlarSatirSayisi)
+  # MySQL sorgusunu çalıştır
+  vtimlec.execute(f"SELECT `gerceklesen`, `fiyat`, `hacim`, `gun` FROM `emirlerim` WHERE `sembol` = '{sembol}' AND `alsat` = 'S'")
+  islemIcinGelenSatimlar = vtimlec.fetchall()
+
+  # Verileri işleme
+  satimVerisiDizeye = []
+  for satir in islemIcinGelenSatimlar:
+    # Toplam alım ve hacimi hesaplama - Dizeye ekleme esnasında yan işlem
+    satimAdet += satir[0]
+    hacim = satir[2] / 1000 * 998
+    satimHacim += hacim
+
+    # Tarih verisini dd mm yyyy formatına çevir
+    tarih = satir[3].strftime("%d %m %Y")
+    # adet verisi tanımıyla gerceklenenleri adete çevir
+    adet = satir[0]
+
+    # Fiyat verisini binlik ayraçlı sayı formatına çevir ve virgülden sonra 2 basamak göster
+    fiyat = '{:,.2f}'.format(satir[1]).replace(",", "X").replace(".", ",").replace("X", ".")
+
+    # Eder verisini binlik ayraçlı sayı formatına çevir ve virgülden sonra 2 basamak göster
+    eder = '{:,.2f}'.format(hacim).replace(",", "X").replace(".", ",").replace("X", ".")
+
+    # Düzenlenmiş veriyi listeye ekle
+    satimVerisiDizeye.append([adet, fiyat, eder, tarih])
+
+  # QTableWidget nesnesine veriyi yerleştir
   kzarayuz.tableWidget_satim.clear()
-  kzarayuz.tableWidget_satim.setHorizontalHeaderLabels(('Adet', 'Fiyat', 'Eder', 'Tarih'))
+  kzarayuz.tableWidget_satim.setHorizontalHeaderLabels([ 'Adet', 'Fiyat', 'Eder', 'Tarih'])
   kzarayuz.tableWidget_satim.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-  kzarayuz.tableWidget_satim.setRowCount(satimlarSatirSayisi)
-  for satirIndeks, satirVeri in enumerate(satimlar):
+  kzarayuz.tableWidget_satim.setRowCount(len(islemIcinGelenSatimlar))
+  for satirIndeks, satirVeri in enumerate(satimVerisiDizeye):
     for sutunIndeks, sutunVeri in enumerate(satirVeri):
-      print(satirVeri[2])
-      kzarayuz.tableWidget_satim.setItem(satirIndeks,sutunIndeks,QTableWidgetItem(str(sutunVeri)))
+      hucre = QTableWidgetItem(str(sutunVeri))
+      if sutunIndeks == 0:
+
+        # Ortala
+        hucre.setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+
+      else:
+
+        # Sağa yasla
+        hucre.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+      if sutunIndeks == 3:
+        hucre.setBackground(QBrush(QColor(242, 237, 221)))
+      # Hücreyi tabloya yerleştir
+      kzarayuz.tableWidget_satim.setItem(satirIndeks, sutunIndeks, hucre)
+
+  # Saydadaki diğer işlemlerde kullanılacak değişken değerlerinin oluşması ve yerleşmesi
   print("Toplam Satılan Adet:" + str(satimAdet))
   satimOrtalamasi = satimHacim / satimAdet
   satimOrtalamasiSade = "{:.2f}".format(satimOrtalamasi)
@@ -97,6 +186,14 @@ def listele_satim():
   kzarayuz.label_satimOrtalama.setText(str(satimOrtalamasiSade))
   global toplamAdet
   toplamAdet -= satimAdet
+  global toplamSatimAdet
+  toplamSatimAdet += satimAdet
+
+def alimAdediYerlestir():
+  kzarayuz.label_alimAdet.setText(str(toplamAlimAdet))
+  kzarayuz.label_satimAdet.setText(str(toplamSatimAdet))
+
+
 
 def adetYerlestir():
   global toplamAdet
@@ -108,10 +205,13 @@ def adetYerlestir():
 
 
 kzmodulAnaPencere.show()
-listele_alim()
-listele_satim()
-adetYerlestir()
-print(toplamAdet)
 
-#print(sembol)
+#alimVerisiIsleme()
+#satimVerisiIsleme()
+sembolleriYerlestir()
+
+#adetYerlestir()
+#alimAdediYerlestir()
+print(toplamAdet)
+#print(satimlariCek)
 sys.exit(Uygulama.exec_())
